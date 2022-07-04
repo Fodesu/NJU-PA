4be3425629dd5af69042517666da276c799090f3
tracer-ics2021 <tracer@njuics.org>
2022-07-03 21:04:07 -0700

    >  compile 20401010103    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: 35a126ceeb0e471fb2fb06b944f5edf3            Boot ID: 8a784d02bacd4b858b600212d65844ea     Virtualization: vmware   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.13.0-52-generic       Architecture: x86-64  21:04:07 up 14:30,  1 user,  load average: 0.14, 0.03, 0.01

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -9,8 +9,8 @@ $(BINARY): compile_git
 
 # Some convenient rules
 
-override ARGS ?= --log=$(BUILD_DIR)/nemu-log.txt
-override ARGS += $(ARGS_DIFF)
+ ARGS ?= --log=$(BUILD_DIR)/nemu-log.txt
+ ARGS += $(ARGS_DIFF)
 
 # Command to execute NEMU
 IMG ?=
