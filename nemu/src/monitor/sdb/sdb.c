b55573b341b2df8cefbbd16e749ad83dedcf777d
tracer-ics2021 <tracer@njuics.org>
2022-07-04 13:47:16 +0800

    >  compile 20401010103    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: 35a126ceeb0e471fb2fb06b944f5edf3            Boot ID: 8a784d02bacd4b858b600212d65844ea     Virtualization: vmware   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.13.0-52-generic       Architecture: x86-64  13:47:16 up 16:13,  1 user,  load average: 0.00, 0.00, 0.00

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -34,6 +34,7 @@ static int cmd_c(char *args) {
 
 
 static int cmd_q(char *args) {
+  nemu_state.state = NEMU_QUIT;
   return -1;
 }
 
@@ -47,12 +48,17 @@ static struct {
   { "help", "Display informations about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
-
+  // { "si", "step into", cmd_si},
   /* TODO: Add more commands */
 
 };
 
 #define NR_CMD ARRLEN(cmd_table)
+
+// static int cmd_si(char *args) {
+// /*                       */
+  
+// }
 
 static int cmd_help(char *args) {
   /* extract the first argument */
