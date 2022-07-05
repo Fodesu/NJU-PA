e990dabf5dd1988d4548e4523cab0cb2987ec5de
tracer-ics2021 <tracer@njuics.org>
2022-07-05 15:46:30 +0800

    >  compile 20401010103    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: 35a126ceeb0e471fb2fb06b944f5edf3            Boot ID: 8a784d02bacd4b858b600212d65844ea     Virtualization: vmware   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.13.0-52-generic       Architecture: x86-64  15:46:30 up 1 day, 18:05,  1 user,  load average: 0.00, 0.00, 0.00

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -109,7 +109,7 @@ static int cmd_x(char *args) {
   }
   int nums = atoi(num);
   for(int i = 0; i < nums; i++) {
-    printf("%x\n", vaddr_read(addr, 4));
+    printf("0x%x\n", vaddr_read(addr, 4));
     addr += 4;
   }
   return 0;
