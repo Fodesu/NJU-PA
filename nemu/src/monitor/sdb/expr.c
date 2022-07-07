14d05ce8f5881ee174dba18c73cc89c068b1bd31
tracer-ics2021 <tracer@njuics.org>
2022-07-07 16:30:45 +0800

    >  compile 20401010103    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: 35a126ceeb0e471fb2fb06b944f5edf3            Boot ID: 8a784d02bacd4b858b600212d65844ea     Virtualization: vmware   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.13.0-52-generic       Architecture: x86-64  16:30:45 up 3 days, 18:50,  1 user,  load average: 0.02, 0.01, 0.00

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -172,7 +172,7 @@ uint32_t Find_Oper(int p, int q) {
       cnt++;
     else if(tokens[i].type == TK_LPARE) 
       cnt--;
-    if(cnt != 0) return -1;
+    if(cnt < 0) return -1;
     if(cnt == 0 && (tokens[i].type == TK_PLUS || tokens[i].type == TK_SUB 
     || tokens[i].type == TK_MULTI || tokens[i].type == TK_DIVI))
       return i;
