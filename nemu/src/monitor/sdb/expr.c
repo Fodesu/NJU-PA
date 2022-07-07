e5af2c843969ceb7d272b8806da3c8d3c39436a1
tracer-ics2021 <tracer@njuics.org>
2022-07-07 13:59:11 +0800

    >  compile 20401010103    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: 35a126ceeb0e471fb2fb06b944f5edf3            Boot ID: 8a784d02bacd4b858b600212d65844ea     Virtualization: vmware   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.13.0-52-generic       Architecture: x86-64  13:59:11 up 3 days, 16:18,  1 user,  load average: 0.01, 0.02, 0.00

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -193,7 +193,7 @@ uint32_t eval(int p, int q, bool* success) {
     if(op == -1) success = false;
     uint32_t val1 = eval(p, op - 1, success);
     uint32_t val2 = eval(op + 1, q, success);
-
+    printf("%d\n", val1 + val2);
     switch (tokens[op].type) {
       case '+': return val1 + val2;
       case '-': return val1 - val2;
