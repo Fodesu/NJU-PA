929f1eeb1626edc05c2a874f99896a0a3d6a58c8
tracer-ics2021 <tracer@njuics.org>
2022-07-08 15:58:01 +0800

    >  compile 20401010103    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: 35a126ceeb0e471fb2fb06b944f5edf3            Boot ID: 8a784d02bacd4b858b600212d65844ea     Virtualization: vmware   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.13.0-52-generic       Architecture: x86-64  15:58:01 up 4 days,  4:42,  1 user,  load average: 0.04, 0.02, 0.00

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -27,7 +27,7 @@ static struct rule {
   {"\\+", TK_PLUS},         // plus
   {"==", TK_EQ},        // equal
   {"^(-[0-9]|[0-9])[0-9]*", TK_DIGITAL},
-  {"(\\?<=[0-9\\)][ ]*)-", TK_SUB},
+  {"(?<=[0-9\\)][ ]*)-", TK_SUB},
   {"\\*", TK_MULTI},
   {"\\/", TK_DIVI},
   {"\\(", TK_LPARE},
